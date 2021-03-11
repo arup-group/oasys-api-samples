5f89a08a37b5b26af407d5d53766ff169a777d71
Richard Gemmell <richard.gemmell@arup.com>
2021-03-11 17:40:49 +0000

    Updated examples to use latest AdSec API build.

--- AdSec/.NET/ReinforcedConcreteAnalysis/ReinforcedConcreteAnalysis.cs
+++ AdSec/.NET/ReinforcedConcreteAnalysis/ReinforcedConcreteAnalysis.cs
@@ -4,6 +4,8 @@ using Oasys.AdSec.DesignCode;
 using Oasys.AdSec.Materials;
 using Oasys.AdSec.Profile;
 using Oasys.AdSec.Reinforcement;
+using UnitsNet;
+using IReinforcement = Oasys.AdSec.Materials.IReinforcement;
 
 namespace ReinforcedConcreteAnalysis
 {
@@ -12,40 +14,40 @@ namespace ReinforcedConcreteAnalysis
         public static void Main()
         {
             // Create a circular section
-            ICircle profile = ICircle.Create(0.5);
+            var profile = ICircle.Create(Length.FromMillimeters(500));
             IConcrete sectionMaterial = Concrete.EN1992.Part1_1.Edition_2004.C40_50;
-            ISection section = ISection.Create(profile, sectionMaterial);
+            var section = ISection.Create(profile, sectionMaterial);
 
             // Set the cover
-            section.Reinforcement.Cover = ICover.Create(0.05);
+            section.Reinforcement.Cover = ICover.Create(Length.FromMillimeters(50));
 
             // Set some reinforcement
-            IReinforcementMat reinforcementMaterial = Reinforcement.Steel.EN1992.Part1_1.Edition_2004.S500B;
-            ILayer layer = ILayerByBarCount.Create(4, reinforcementMaterial, 0.032);
-            ILayeredGroup group = ILine.Create(IPoint.Create(0, 0), IPoint.Create(0.15, 0.15), layer);
+            IReinforcement reinforcementMaterial = Reinforcement.Steel.EN1992.Part1_1.Edition_2004.S500B;
+            ILayer layer = ILayerByBarCount.Create(4, reinforcementMaterial, Length.FromMillimeters(32));
+            ILayeredGroup group = ILine.Create(IPoint.Create(Length.Zero, Length.Zero), IPoint.Create(Length.FromMillimeters(150), Length.FromMillimeters(150)), layer);
             group.Layers.Add(layer);
             section.Reinforcement.Groups.Add(group);
 
             // Analyse the section to create a solution
-            IAdSec adSec = IAdSec.Create(EN1992.Part1_1.Edition_2004.NationalAnnex.GB.Edition_2014);
+            var adSec = IAdSec.Create(EN1992.Part1_1.Edition_2004.NationalAnnex.GB.Edition_2014);
             ISolution solution = adSec.Analyse(section);
 
             // Calculate utilisation for a particular load
-            var axialForce = UnitsNet.Force.FromKilonewtons(-100);
-            var majorAxisBending = 60000;
-            var minorAxisBending = 0;
+            var axialForce = Force.FromKilonewtons(-100);
+            var majorAxisBending = Moment.FromKilonewtonMeters(60);
+            var minorAxisBending = Moment.Zero;
             var load = ILoad.Create(axialForce, majorAxisBending, minorAxisBending);
             IStrengthResult strengthResult = solution.Strength.Check(load);
 
             // Display utilisation as a percentage
-            double utilisation = Math.Round(strengthResult.LoadUtilisation * 100, 1);
+            double utilisation = Math.Round(strengthResult.LoadUtilisation.Percent, 1);
             Console.WriteLine($"The utilisation is: {utilisation}%");
 
             // Calculate the serviceability crack width under the same load
             IServiceabilityResult serviceabilityResult = solution.Serviceability.Check(load);
 
             // Display the crack width in mm
-            double crackWidth = Math.Round(serviceabilityResult.MaximumWidthCrack.Width * 1000, 2);
+            double crackWidth = Math.Round(serviceabilityResult.MaximumWidthCrack.Width.Millimeters, 2);
             Console.WriteLine($"The maximum crack width is: {crackWidth}mm");
         }
     }
--- AdSec/Python/reinforced_concrete_analysis.py
+++ AdSec/Python/reinforced_concrete_analysis.py
@@ -9,24 +9,39 @@ from Oasys.AdSec.DesignCode import EN1992
 from Oasys.AdSec.Materials import Concrete, Reinforcement
 from Oasys.AdSec.Profile import ICircle, IPoint
 from Oasys.AdSec.Reinforcement import ICover, ILayerByBarCount, ILine
-import UnitsNet
-from UnitsNet import QuantityValue
+from UnitsNet import Force, Length, Moment, QuantityValue
 from PythonNetHelpers import TypeHelpers
+
+
+def quantity_value(value) -> QuantityValue:
+    return TypeHelpers.Cast[QuantityValue](value)
+
+
+def mm(distance) -> Length:
+    return Length.FromMillimeters(quantity_value(distance))
+
+
+def kN(force) -> Force:
+    return Force.FromKilonewtons(quantity_value(force))
+
+
+def kNm(force) -> Moment:
+    return Moment.FromKilonewtonMeters(quantity_value(force))
 
 
 def strength_analysis():
     # Create a circular section
-    profile = ICircle.Create(0.5)
+    profile = ICircle.Create(mm(500))
     sectionMaterial = Concrete.EN1992.Part1_1.Edition_2004.C40_50
     section = ISection.Create(profile, sectionMaterial)
 
     # Set the cover
-    section.Reinforcement.Cover = ICover.Create(0.05)
+    section.Reinforcement.Cover = ICover.Create(mm(50))
 
     # Set some reinforcement
     reinforcementMaterial = Reinforcement.Steel.EN1992.Part1_1.Edition_2004.S500B
-    layer = ILayerByBarCount.Create(4, reinforcementMaterial, 0.032)
-    group = ILine.Create(IPoint.Create(0, 0), IPoint.Create(0.15, 0.15), layer)
+    layer = ILayerByBarCount.Create(4, reinforcementMaterial, mm(32))
+    group = ILine.Create(IPoint.Create(Length.Zero, Length.Zero), IPoint.Create(mm(150), mm(150)), layer)
     group.Layers.Add(layer)
     section.Reinforcement.Groups.Add(group)
 
@@ -35,21 +50,21 @@ def strength_analysis():
     solution = adSec.Analyse(section)
 
     # Calculate utilisation for a particular load
-    axialForce = UnitsNet.Force.FromKilonewtons(TypeHelpers.Cast[QuantityValue](-100))
-    majorAxisBending = 60000
-    minorAxisBending = 0
+    axialForce = kN(-100)
+    majorAxisBending = kNm(60)
+    minorAxisBending = Moment.Zero
     load = ILoad.Create(axialForce, majorAxisBending, minorAxisBending)
     strengthResult = solution.Strength.Check(load)
 
     # Display utilisation as a percentage
-    utilisation = round(strengthResult.LoadUtilisation * 100, 1)
+    utilisation = round(strengthResult.LoadUtilisation.Percent, 1)
     print("The utilisation is: " + str(utilisation) + "%")
 
     # Calculate the serviceability crack width under the same load
     serviceabilityResult = solution.Serviceability.Check(load)
 
     # Display the crack width in mm
-    crackWidth = round(serviceabilityResult.MaximumWidthCrack.Width * 1000, 2)
+    crackWidth = round(serviceabilityResult.MaximumWidthCrack.Width.Millimeters, 2)
     print("The maximum crack width is: " + str(crackWidth) + " mm")
 
 
