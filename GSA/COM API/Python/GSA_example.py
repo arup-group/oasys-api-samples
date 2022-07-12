418a52873182126195b00f15a0428bfcf7656498
GLOBAL\Peter.Debney <peter.debney@arup.com>
2022-07-12 17:23:32 +0100

    GSA Python and gsapy/Compos sample files

---
+++ GSA/COM API/Python/GSA_example.py
@@ -0,0 +1,16 @@
+import win32com.client  # https://pypi.org/project/pywin32/
+import requests  # https://pypi.org/project/requests/    HTTP library
+
+gsa_obj = win32com.client.Dispatch("Gsa_10_1.ComAuto")
+
+sample_model = requests.get("https://samples.oasys-software.com/gsa/10.1/General/Stair.gwb",
+                            allow_redirects=True)
+open('c:\\Temp\\Stair.gwb', 'wb').write(sample_model.content)  # saves sample_model in Temp folder
+gsa_obj.Open("c:\\Temp\\Stair.gwb")
+gsa_obj.Delete("RESULTS")
+gsa_obj.Analyse()
+gsa_obj.SaveAs("c:\\Temp\\stair_analysed.gwb")
+gsa_obj.Close()
+gsa_obj = None
+
+print("finished")
