52a72e4e7acffe944f1efc7a375c6ecf0602548d
GLOBAL\Peter.Debney <peter.debney@arup.com>
2022-07-19 11:56:50 +0100

    Update to remove GSA2Compos sample and tweak GSA_example

--- GSA/COM API/Python/GSA_example.py
+++ GSA/COM API/Python/GSA_example.py
@@ -4,9 +4,11 @@ import requests  # https://pypi.org/project/requests/    HTTP library
 gsa_obj = win32com.client.Dispatch("Gsa_10_1.ComAuto")
 
 print("Opening sample stair model")
-sample_model = requests.get("https://samples.oasys-software.com/gsa/10.1/General/Stair.gwb",
-                            allow_redirects=True)
-open('c:\\Temp\\Stair.gwb', 'wb').write(sample_model.content)  # saves sample_model in Temp folder
+sample_model = requests.get(
+    "https://samples.oasys-software.com/gsa/10.1/General/Stair.gwb",
+    allow_redirects=True)
+# saves sample_model in Temp folder
+open('c:\\Temp\\Stair.gwb', 'wb').write(sample_model.content)
 gsa_obj.Open("c:\\Temp\\Stair.gwb")
 gsa_obj.Delete("RESULTS")
 gsa_obj.Analyse()
