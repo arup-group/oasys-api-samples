2a952d642a7efb8bc8fe457b96ca60e54e665c7d
Anish Sanghai <Anish.Sanghai@arup.com>
2024-05-01 11:47:15 +0530

    [ADSEC-2019] Apply pre-commit to the files
    Signed-off-by: Anish Sanghai <Anish.Sanghai@arup.com>

--- AdSec/.NET/ApiToExcel/ApiToExcel.cs
+++ AdSec/.NET/ApiToExcel/ApiToExcel.cs
@@ -1,8 +1,8 @@
-﻿//Importing the necessary packages
+//Importing the necessary packages
 using System;
 using System.IO;
 using System.Linq;
-using ClosedXML.Excel;//Open source library That can be used for reading and writing in the excel file 
+using ClosedXML.Excel; //Open source library That can be used for reading and writing in the excel file
 using Oasys.AdSec;
 using OasysUnits;
 using OasysUnits.Units;
@@ -25,30 +25,38 @@ class Program
             // creating adsec object and defining section material
             var sectionMaterial = Steel.EN1993.Edition_2005.S235;
             var adSec = IAdSec.Create(EN1992.Part1_1.Edition_2004.NationalAnnex.GB.Edition_2014);
-            //Creating a secion 
+            //Creating a secion
             var diameter = new Length(80, LengthUnit.Inch);
             var circleProfile = ICircleProfile.Create(diameter);
             var section = ISection.Create(circleProfile, sectionMaterial);
             //Analysing the section
             var circleSectionAnalysis = adSec.Analyse(section);
-            // Starting from 2 as the 1st row is the heading 
+            // Starting from 2 as the 1st row is the heading
             int x = 2;
             for (int i = 2; i < load.Count(); i++)
-            { 
-                // Getting the loads and coverting it 
-                var force_x = new Force((double)worksheet.Cell("A" + i).Value, ForceUnit.Kilonewton);
-                var moment_yy = new Moment((double)worksheet.Cell("B" + i).Value, MomentUnit.KilonewtonMeter);
-                var moment_zz = new Moment((double)worksheet.Cell("C" + i).Value, MomentUnit.KilonewtonMeter);
+            {
+                // Getting the loads and coverting it
+                var force_x = new Force(
+                    (double)worksheet.Cell("A" + i).Value,
+                    ForceUnit.Kilonewton
+                );
+                var moment_yy = new Moment(
+                    (double)worksheet.Cell("B" + i).Value,
+                    MomentUnit.KilonewtonMeter
+                );
+                var moment_zz = new Moment(
+                    (double)worksheet.Cell("C" + i).Value,
+                    MomentUnit.KilonewtonMeter
+                );
                 var circleLoad = ILoad.Create(force_x, moment_yy, moment_zz);
                 //Getting the strength results
                 var strengthResult = circleSectionAnalysis.Strength.Check(circleLoad);
                 var utilisation = Math.Round(strengthResult.LoadUtilisation.Percent, 1);
                 var ulsStatus = utilisation < 100 ? "Clear" : "Not Clear";
-                worksheet.Cell("D" + i).Value = utilisation; 
-                worksheet.Cell("E" + i).Value = ulsStatus;  
+                worksheet.Cell("D" + i).Value = utilisation;
+                worksheet.Cell("E" + i).Value = ulsStatus;
             }
             workbook.Save();
-
         }
         catch (FileNotFoundException)
         {
--- AdSec/Python/ApiToExcel.py
+++ AdSec/Python/ApiToExcel.py
@@ -1,49 +1,46 @@
-#importing packages
-import oasys.adsec
-from OasysUnits import Force, Length, Moment
-from OasysUnits.Units import LengthUnit,ForceUnit,MomentUnit
-from Oasys.Profiles import ICircleProfile
-from Oasys.AdSec.StandardMaterials import Steel
-from Oasys.AdSec import ISection
-from Oasys.AdSec import IAdSec,ILoad
+# importing packages
+import openpyxl as op  # OPen source library that can be used for reading and writing into the excel files
+from Oasys.AdSec import IAdSec, ILoad, ISection
 from Oasys.AdSec.DesignCode import EN1992
-import openpyxl as op#OPen source library that can be used for reading and writing into the excel files
+from Oasys.AdSec.StandardMaterials import Steel
+from Oasys.Profiles import ICircleProfile
+from OasysUnits import Force, Length, Moment
+from OasysUnits.Units import ForceUnit, LengthUnit, MomentUnit
 
-#Reading Values from the excel file
+# Reading Values from the excel file
 
 try:
-    workbook=op.load_workbook(filename='C:\Repo\oasys-api-samples\AdSec\Python\loads.xlsx')
-    sheet1=workbook['Sheet1']
-    load=[row for row in sheet1.iter_rows(min_row=2,max_col=3, values_only=True)]
-    #Defining the Section Material
-    sectionMaterial = Steel.EN1993.Edition_2005.S235
+    workbook = op.load_workbook(filename=r"C:\Repo\oasys-api-samples\AdSec\Python\loads.xlsx")
+    sheet1 = workbook["Sheet1"]
+    load = [row for row in sheet1.iter_rows(min_row=2, max_col=3, values_only=True)]
+    # Defining the Section Material
+    section_material = Steel.EN1993.Edition_2005.S235
 
-    #Creating adsec object
-    adSec = IAdSec.Create(EN1992.Part1_1.Edition_2004.NationalAnnex.GB.Edition_2014)
+    # Creating adsec object
+    adsec = IAdSec.Create(EN1992.Part1_1.Edition_2004.NationalAnnex.GB.Edition_2014)
 
-    #Creating a Circular Section
+    # Creating a Circular Section
     diameter = Length(float(80), LengthUnit.Inch)
-    CircleProfile = ICircleProfile.Create(diameter)
-    section = ISection.Create(CircleProfile, sectionMaterial)
+    circle_profile = ICircleProfile.Create(diameter)
+    section = ISection.Create(circle_profile, section_material)
 
-    #circular Section Analysis
-    circle_section_Analyse = adSec.Analyse(section)
-    x=2
+    # circular Section Analysis
+    circle_section_analyse = adsec.Analyse(section)
+    x = 2
+    max_utilization = 100
     for row in load:
         Force_x = Force(float(row[0]), ForceUnit.Kilonewton)
         Moment_yy = Moment(float(row[1]), MomentUnit.KilonewtonMeter)
         Moment_zz = Moment(float(row[2]), MomentUnit.KilonewtonMeter)
         circle_load = ILoad.Create(Force_x, Moment_yy, Moment_zz)
-        strengthResult = circle_section_Analyse.Strength.Check(circle_load)
-        utilisation = round(strengthResult.LoadUtilisation.Percent, 1)
-        ULS_Status= "Clear :)" if utilisation<100 else "Not Clear :("
-        sheet1['D'+str(x)]=utilisation
-        sheet1['E'+str(x)]=ULS_Status
-        x+=1
-    workbook.save('loads.xlsx')
+        strength_result = circle_section_analyse.Strength.Check(circle_load)
+        utilisation = round(strength_result.LoadUtilisation.Percent, 1)
+        uls_status = "Clear :)" if utilisation < max_utilization else "Not Clear :("
+        sheet1["D" + str(x)] = utilisation
+        sheet1["E" + str(x)] = uls_status
+        x += 1
+    workbook.save("loads.xlsx")
 except FileNotFoundError:
     print("file not found")
 except Exception as e:
-    print("Error occured: ",e);
-    
-
+    print("Error occured: ", e)
